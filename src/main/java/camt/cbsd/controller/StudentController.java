f113e6cb44d9795bb9f5e0f116aa28d70094f0b8
DESKTOP-9CKSS1Q\CAMT <zuckerwatte.91@gmail.com>
2017-03-31 16:13:49 +0700

    update lab

--- src/main/java/camt/cbsd/controller/StudentController.java
+++ src/main/java/camt/cbsd/controller/StudentController.java
@@ -4,29 +4,43 @@ import camt.cbsd.entity.Student;
 import camt.cbsd.services.StudentService;
 import camt.cbsd.services.StudentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.File;
+import java.nio.file.Paths;
 import java.util.List;
-
+@ConfigurationProperties(prefix="server")
 @Component
 @Path("/student")
 public class StudentController {
     StudentService studentService;
+    private String imageServerDir;
+
+    public void setImageServerDir(String imageServerDir) {
+        this.imageServerDir = imageServerDir;
+    }
+
     @Autowired
     public void setStudentService(StudentService studentService) {
         this.studentService = studentService;
     }
 
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response uploadOnlyStudent(Student student){
+        System.out.println(student);
+        return Response.ok().entity(student).build();
+    }
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getStudents(){
-
         List<Student> students = studentService.getStudents();
         return Response.ok(students).build();
     }
@@ -43,4 +57,18 @@ public class StudentController {
             return Response.status(Response.Status.NO_CONTENT).build();
 
     }
+    @GET
+    @Path("/images/{fileName}")
+    @Produces({"image/png","image/jpg","image/gif"})
+    public Response getStudentImage(@PathParam("fileName") String filename){
+        File file = Paths.get(imageServerDir+filename).toFile();
+        Response.ResponseBuilder responseBuilder = Response.ok((Object) file);
+        if (file.exists()){
+            responseBuilder.header("Content-Disposition", "attachment;filename=\"a.jpg\"");
+        }else{return Response.status(Response.Status.NOT_FOUND).build();}
+
+        return responseBuilder.build();
+
+    }
+
 }
--- src/main/java/camt/cbsd/entity/Student.java
+++ src/main/java/camt/cbsd/entity/Student.java
@@ -1,10 +1,18 @@
 package camt.cbsd.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /**
  * Created by Dto on 3/11/2017.
  */
+@JsonIgnoreProperties(ignoreUnknown =true)
+
 public class Student {
     long id;
+
+    public Student() {
+    }
+
     String studentId;
     String name;
     String surname;
